7ed72ea66fd610ea770b4ff0c7401b0d606097fa
JousefAly <youssefali00286@gmail.com>
2022-06-25 17:43:40 +0200

    created project to practice Sorting

---
+++ Practice Alogrithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_Alogrithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
