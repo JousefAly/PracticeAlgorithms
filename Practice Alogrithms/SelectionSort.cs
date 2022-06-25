8eef8bb31e5f6fa8fe5f80fc446ec195afedb431
JousefAly <youssefali00286@gmail.com>
2022-06-25 18:32:07 +0200

    started coding

---
+++ Practice Alogrithms/SelectionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Alogrithms
+{
+    public class SelectionSort
+    {
+        public static int[] Sort(int[] nums)
+        {
+            
+        }
+
+        private int findSmallest(int[] nums)
+        {
+            int smallest = int.MaxValue;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < smallest)
+                    smallest = nums[i];
+            }
+        }
+    }
+}
