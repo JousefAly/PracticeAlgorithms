d1b0df8fa3fc7545120b99718a62352e653e7454
JousefAly <youssefali00286@gmail.com>
2022-07-30 18:58:28 +0200

    SumRecursionStart

---
+++ Practice/SumRecursion/ConsoleApp1/SumFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class SumFunction
+    {
+        public static int Sum(int[] arr)
+        {
+            if (arr.Length == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return arr[0] + Sum(arr);
+            }
+        }
+    }
+}
